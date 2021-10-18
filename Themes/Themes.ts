002b262215ea942db123ce4f1517671c4303437f
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-18 20:23:45 +0500

    Themes for ThemeProvider

---
+++ Themes/Themes.ts
@@ -0,0 +1,23 @@
+import { Theme } from '../types/Theme.types'
+
+export const ligth: Theme = {
+  colors: {
+    font: 'red',
+  },
+  fontSize: {
+    small: '',
+    medium: '',
+    large: '',
+  },
+}
+
+export const dark: Theme = {
+  colors: {
+    font: 'black',
+  },
+  fontSize: {
+    small: '',
+    medium: '',
+    large: '',
+  },
+}
---
+++ types/Theme.types.ts
@@ -0,0 +1,10 @@
+export interface Theme {
+  colors: {
+    font: string
+  }
+  fontSize: {
+    small: string
+    medium: string
+    large: string
+  }
+}
