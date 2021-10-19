e0eb69eafc6104317fc0988b5e5f0335aa8a8e8d
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-19 23:23:01 +0500

    themes object

---
+++ styles/themes.ts
@@ -0,0 +1,50 @@
+import { Color, Theme } from "../types/Theme.types"
+
+const colors: {
+  [key: string]: {
+    [key: string]: Color
+  }
+} = {
+  text: {
+    primary: '#202020',
+    secondary: '#7E7E7E',
+    danger: '#F0452D',
+    white: '#FBFBFB',
+  },
+  background: {
+    disabled: '#F2F2F2',
+    danger: 'linear-gradient(90.26deg, #F85032 -2.18%, #E73827 99.78%)',
+    primary: 'linear-gradient(90.26deg, #DE6262 -2.18%, #FFB88C 99.78%)',
+    white: '#FBFBFB'
+  },
+}
+
+const themes: {
+  light: Theme
+  dark: any
+} = {
+  light: {
+    background : '#FBFBFB',
+    placeholder: colors.text.secondary,
+    buttons: {
+      color: {
+        text: {
+          disabled: colors.text.secondary,
+          danger: colors.text.textSecondary,
+          primary: colors.text.white,
+          secondary: colors.text.secondary,
+          link: colors.text.primary
+        },
+        background: {
+          disabled: colors.background.disabled,
+          danger: colors.background.danger,
+          primary: colors.background.primary,
+          secondary: colors.background.secondary,
+        }
+      }
+    }
+  },
+  dark: ''
+}
+
+export default themes
--- types/Theme.types.ts
+++ types/Theme.types.ts
@@ -1,10 +1,39 @@
+export type Color =
+  | '#F2F2F2'
+  | '#202020'
+  | '#7E7E7E'
+
+  // white
+  | '#FBFBFB'
+
+  // danger
+  | '#F0452D'
+
+  // danger background
+  | 'linear-gradient(90.26deg, #F85032 -2.18%, #E73827 99.78%)'
+
+  // primary background
+  | 'linear-gradient(90.26deg, #DE6262 -2.18%, #FFB88C 99.78%)'
+
+
 export interface Theme {
-  colors: {
-    font: string
-  }
-  fontSize: {
-    small: string
-    medium: string
-    large: string
+  background: Color
+  placeholder: Color
+  buttons: {
+    color: {
+      text: {
+        disabled: Color
+        danger: Color
+        primary: Color
+        secondary: Color
+        link: Color
+      }
+      background: {
+        disabled: Color
+        danger: Color
+        primary: Color
+        secondary: Color
+      }
+    }
   }
 }
