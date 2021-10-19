66cc1a311479a397a035cdd8922a2ffb5c2ada04
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-19 23:24:10 +0500

    Auth Component: added buttons and inputs

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -6,6 +6,11 @@ import { AuthForm } from '../../types/Auth.types'
 
 import { useHttp } from '../../hooks/http.hook'
 
+import Button from '../UI/Button/Button'
+import Input from '../UI/Input/Input'
+
+import { Wrapper } from './AuthStyles'
+
 interface Props {}
 
 const Auth: React.FC<Props> = () => {
@@ -47,47 +52,83 @@ const Auth: React.FC<Props> = () => {
   const changeHandler = event => {
     setForm({ ...form, [event.target.name]: event.target.value })
   }
-  // TODO: Input components
-  // TODO: Button
+
   return (
-    <>
+    <Wrapper>
       {isLoggingIn ? (
         <>
-          <input
-            name={'email'}
-            onChange={changeHandler}
-            placeholder={L.email}
-          />
-          <input
-            name={'password'}
-            onChange={changeHandler}
-            placeholder={L.password}
-          />
-          <button onClick={loginHandle}>{L.logIn}</button>
-          <button onClick={isLoggingInHandle}>{L.createAccount}</button>
+          <div className='Wrapper__inputsContainer'>
+            <Input
+              placeholder={L.email}
+              size={'normal'}
+              onChange={changeHandler}
+              name={'email'}
+            />
+            <Input
+              placeholder={L.password}
+              size={'normal'}
+              onChange={changeHandler}
+              name={'password'}
+              type={'password'}
+            />
+          </div>
+          <div className='Wrapper__buttonsContainer'>
+            <Button
+              clickHandler={loginHandle}
+              text={L.logIn}
+              size={'normal'}
+              type={form.email && form.password ? 'primary' : 'secondary'}
+            />
+            <Button
+              clickHandler={isLoggingInHandle}
+              text={L.createAccount}
+              size={'normal'}
+              type={'link'}
+            />
+          </div>
         </>
       ) : (
         <>
-          <input
-            name={'email'}
-            onChange={changeHandler}
-            placeholder={L.email}
-          />
-          <input
-            name={'password'}
-            onChange={changeHandler}
-            placeholder={L.password}
-          />
-          <input
-            name={'nickname'}
-            onChange={changeHandler}
-            placeholder={L.nickname}
-          />
-          <button onClick={registerHandle}>{L.register}</button>
-          <button onClick={isLoggingInHandle}>{L.iHaveAccount}</button>
+          <div className='Wrapper__inputsContainer'>
+            <Input
+              placeholder={L.email}
+              size={'normal'}
+              onChange={changeHandler}
+              name={'email'}
+            />
+            <Input
+              placeholder={L.password}
+              size={'normal'}
+              onChange={changeHandler}
+              name={'password'}
+              type={'password'}
+            />
+            <Input
+              placeholder={L.nickname}
+              size={'normal'}
+              onChange={changeHandler}
+              name={'nickname'}
+            />
+          </div>
+          <div className='Wrapper__buttonsContainer'>
+            <Button
+              clickHandler={registerHandle}
+              text={L.register}
+              size={'normal'}
+              type={
+                form.email && form.password && form.nickname ? 'primary' : 'secondary'
+              }
+            />
+            <Button
+              clickHandler={isLoggingInHandle}
+              text={L.iHaveAccount}
+              size={'normal'}
+              type={'link'}
+            />
+          </div>
         </>
       )}
-    </>
+    </Wrapper>
   )
 }
 
