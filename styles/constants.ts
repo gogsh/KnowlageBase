a00b79efc5fad52fc5024f636399791bc9f74a2b
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-19 23:22:13 +0500

    style constants

---
+++ styles/constants.ts
@@ -0,0 +1,18 @@
+export const constants = {
+  borderRadius: '6px',
+  font: {
+    small: '18px',
+    normal: '24px',
+    large: '30px',
+  },
+  buttonsPadding: {
+    small: '4px 10px 6px 10px',
+    normal: '8px 12px 12px 12px',
+    large: '8px 14px 10px 14px',
+  },
+  inputsPadding: {
+    small: '4px 0px 6px 0px',
+    normal: '8px 0px 10px 0px',
+    large: '8px 0px 10px 0px',
+  }
+}
