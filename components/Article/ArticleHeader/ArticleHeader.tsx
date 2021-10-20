49960f56d09f2011e9d0b68bdc5862dc4ef60024
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-20 23:24:09 +0500

    ArticleHeader Component

---
+++ components/Article/ArticleHeader/ArticleHeader.tsx
@@ -0,0 +1,15 @@
+import Header from '../../UI/Header/Header'
+interface Props {}
+
+function ArticleHeader(props: Props) {
+  const {} = props
+
+  return (
+      <Header>
+        <h2>Main</h2>
+        <i>door</i>
+      </Header>
+  )
+}
+
+export default ArticleHeader
