dc2c1e93ec867bf90dfe49d0f5ce1af07533b4c8
Ilya <IlyaAbdullinWork@yandex.ru>
2021-10-22 23:35:35 +0500

    ArticleHeader Component: added icons and styles

--- components/Article/ArticleHeader/ArticleHeader.tsx
+++ components/Article/ArticleHeader/ArticleHeader.tsx
@@ -1,14 +1,19 @@
 import Header from '../../UI/Header/Header'
+import Icon from '../../UI/Icon/Icon'
+import { MenuWrapper } from './ArticleHeaderStyle'
 interface Props {}
 
 function ArticleHeader(props: Props) {
   const {} = props
-
+  
   return (
-      <Header>
-        <h2>Main</h2>
-        <i>door</i>
-      </Header>
+    <Header>
+      <h2>Main</h2>
+      <MenuWrapper>
+        <Icon type={'cog'} clickHandler={() => {console.log(1)}} />
+        <Icon type={'door'} clickHandler={() => {console.log(1)}} />
+      </MenuWrapper>
+    </Header>
   )
 }
 
--- components/Article/ArticleHeader/ArticleHeaderStyle.ts
+++ components/Article/ArticleHeader/ArticleHeaderStyle.ts
@@ -1,2 +1,9 @@
 import styled from 'styled-components'
 
+export const MenuWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  gap: 16px;
+`
